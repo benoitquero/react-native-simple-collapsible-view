8135709b66dcb8db54d8506ac350e40bac00202c
Benoit Quero <benquero2@hotmail.fr>
2023-12-30 16:14:26 +0100

    feat: update npm generated files

---
+++ lib/typescript/src/ArrowIcon.d.ts
@@ -0,0 +1,6 @@
+import React from 'react';
+export interface ArrowIconStyling {
+    size: number;
+}
+export default function ArrowIcon({ size }: ArrowIconStyling): React.JSX.Element;
+//# sourceMappingURL=ArrowIcon.d.ts.map
--- lib/typescript/src/index.d.ts
+++ lib/typescript/src/index.d.ts
@@ -1,2 +1,17 @@
-export declare function multiply(a: number, b: number): Promise<number>;
+import React from 'react';
+import { type ViewStyle } from 'react-native';
+import type { TextStyle } from 'react-native';
+interface CollapsibleViewProps {
+    animated?: boolean;
+    arrowIcon?: React.ReactNode;
+    arrowSize?: number;
+    children: React.ReactNode;
+    containerStyle?: ViewStyle;
+    defaultState?: 'open' | 'closed';
+    headerContainerStyle?: ViewStyle;
+    title: string;
+    titleStyle?: TextStyle;
+}
+export declare const CollapsibleView: ({ children, title, arrowIcon, arrowSize, titleStyle, containerStyle, headerContainerStyle, animated, defaultState, }: CollapsibleViewProps) => React.JSX.Element;
+export {};
 //# sourceMappingURL=index.d.ts.map
